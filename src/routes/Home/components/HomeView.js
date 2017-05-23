166f052a97ef4dbd343f8de03d98c6b260c24e57
janeluck <janeluck158@gmail.com>
2017-05-23 15:41:56 +0800

    chaoke h5

---
+++ src/routes/Home/components/HomeView.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import DuckImage from '../assets/Duck.jpg'
+import './HomeView.scss'
+import {Button} from 'antd-mobile'
+export const HomeView = () => (
+  <div>
+    <h4>Welcome</h4>
+    <Button>button</Button>
+      <Button className="btn" type="primary">primary button</Button>
+        <Button className="btn" disabled onClick={e => console.log(e)}>
+          disabled button
+        </Button>
+        <Button className="btn" loading>loading button</Button>
+        <Button className="btn" icon="check-circle-o">with icon</Button>
+
+    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+  </div>
+)
+
+export default HomeView
