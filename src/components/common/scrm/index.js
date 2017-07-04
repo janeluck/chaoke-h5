345d3d393dc69df9356084090df9834894a7f605
janeluck <janeluck158@gmail.com>
2017-07-04 11:39:36 +0800

    goBack

--- src/components/common/scrm/index.js
+++ src/components/common/scrm/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by janeluck on 17/6/29.
  */
+import _ from 'lodash'
 const scrm = {
   //todo: 根据token具体用途还需要再改
   url: function (str) {
@@ -15,7 +16,7 @@ const scrm = {
   },
   // 返回app内部
   goBack: function () {
-    window.jsObj && window.jsObj.goBack && window.jsObj.goBack()
+    window.jsObj && _.isFunction(window.jsObj.goBack) && window.jsObj.goBack()
   }
 }
 
